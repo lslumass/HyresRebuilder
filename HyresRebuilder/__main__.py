73fd5792dd63ec714e43d67305f05ff5b776345a
Shanlong Li <shanlongli@umass.edu>
2023-10-26 15:31:20 -0400

    Update __main__.py

--- HyresRebuilder/__main__.py
+++ HyresRebuilder/__main__.py
@@ -1,8 +1,13 @@
 import sys
+import argparse
 from .Rebuilder import rebuild
 
+parser = argparse.ArgumentParser(
+  prog='HyresRebuilder',
+  description='Rebuild atomistic model from HyRes model.'
+)
+parser.add_argument('input')
+parser.add_argument('output')
 
 if __name__ == '__main__':
-  inp = sys.argv[1]
-  out = sys.argv[2]
-  rebuild(inp, out)
+  rebuild(parser.input, parser.output)
