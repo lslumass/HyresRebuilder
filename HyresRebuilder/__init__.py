e69bfc42d8f0be8210de02695cfa92307042beb8
Shanlong Li <shanlongli@umass.edu>
2023-10-26 09:24:44 -0400

    Create __init__.py

---
+++ HyresRebuilder/__init__.py
@@ -0,0 +1,12 @@
+""" HyresRebuilder
+Written by Shanlong Li
+
+Python modules
+----------------
+The package consists of the following Python modules:
+* HyresRebuilder
+* Rotamer
+"""
+__version__ = "1.2.7"
+from .HyresBuilder import *
+from .Rotamer import *
