651100999e344fde40fdf7c3c9ace56eafedf71f
Shanlong Li <shanlongli@umass.edu>
2023-10-26 16:40:33 -0400

    Update __init__.py

--- HyresRebuilder/__init__.py
+++ HyresRebuilder/__init__.py
@@ -7,7 +7,7 @@ The package consists of the following Python modules:
 * HyresRebuilder
 * Rotamer
 """
-__version__ = "0.3.5"
+__version__ = "0.3.6"
 from .Rebuilder import *
 from .Rotamer import *
 from .base import *
